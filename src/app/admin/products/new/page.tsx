a930ccacbc331898b506a98918c41846900748a8
Jude Skrzat <jskrzat@kent.edu>
2025-09-28 14:36:27 -0400

    working on products.ts

---
+++ src/app/admin/products/new/page.tsx
@@ -0,0 +1,9 @@
+import { PageHeader } from "../../_components/pageHeader";
+import { ProductForm } from "../_components/ProductsForm";
+
+export default function NewProductPage() {
+    return <>
+    <PageHeader>Add Product</PageHeader>
+    <ProductForm />
+    </>
+}
