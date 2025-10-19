5ecfdf2b1b44183a1766dcbeda626c274134725e
Jude Skrzat <jskrzat@kent.edu>
2025-10-19 15:52:06 -0400

    EVERYTHING FINISHED!!!!!

--- src/app/admin/orders/page.tsx
+++ src/app/admin/orders/page.tsx
@@ -7,63 +7,62 @@ import {
   TableRow,
 } from "@/components/ui/table"
 import db from "@/db/db"
-import { formatCurrency, formatNumber } from "@/lib/formatters"
+import { formatCurrency } from "@/lib/formatters"
 import {
   DropdownMenu,
   DropdownMenuContent,
+  DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { MoreVertical } from "lucide-react"
-import { DeleteDropDownItem } from "./_components/UserActions"
+import { DeleteDropDownItem } from "./_components/OrderActions"
 import { PageHeader } from './../_components/pageHeader';
 
-function getUsers() {
-  return db.user.findMany({
+function getOrders() {
+  return db.order.findMany({
     select: {
       id: true,
-      email: true,
-      orders: { select: { pricePaidInCents: true } },
+      pricePaidInCents: true,
+      product: { select: { name: true } },
+      user: { select: { email: true } },
     },
     orderBy: { createdAt: "desc" },
   })
 }
 
-export default function UsersPage() {
+export default function OrdersPage() {
   return (
     <>
-      <PageHeader>Customers</PageHeader>
-      <UsersTable />
+      <PageHeader>Sales</PageHeader>
+      <OrdersTable />
     </>
   )
 }
 
-async function UsersTable() {
-  const users = await getUsers()
+async function OrdersTable() {
+  const orders = await getOrders()
 
-  if (users.length === 0) return <p>No customers found</p>
+  if (orders.length === 0) return <p>No sales found</p>
 
   return (
     <Table>
       <TableHeader>
         <TableRow>
-          <TableHead>Email</TableHead>
-          <TableHead>Orders</TableHead>
-          <TableHead>Value</TableHead>
+          <TableHead>Product</TableHead>
+          <TableHead>Customer</TableHead>
+          <TableHead>Price Paid</TableHead>
           <TableHead className="w-0">
             <span className="sr-only">Actions</span>
           </TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
-        {users.map(user => (
-          <TableRow key={user.id}>
-            <TableCell>{user.email}</TableCell>
-            <TableCell>{formatNumber(user.orders.length)}</TableCell>
+        {orders.map(order => (
+          <TableRow key={order.id}>
+            <TableCell>{order.product.name}</TableCell>
+            <TableCell>{order.user.email}</TableCell>
             <TableCell>
-              {formatCurrency(
-                user.orders.reduce((sum, o) => o.pricePaidInCents + sum, 0) /
-                  100
-              )}
+              {formatCurrency(order.pricePaidInCents / 100)}
             </TableCell>
             <TableCell className="text-center">
               <DropdownMenu>
@@ -72,7 +71,7 @@ async function UsersTable() {
                   <span className="sr-only">Actions</span>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent>
-                  <DeleteDropDownItem id={user.id} />
+                  <DeleteDropDownItem id={order.id} />
                 </DropdownMenuContent>
               </DropdownMenu>
             </TableCell>
